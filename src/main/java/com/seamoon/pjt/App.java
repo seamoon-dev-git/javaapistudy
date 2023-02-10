0d0c0f42394af4e235242f2229b7727b32d4a86e
SeaMoon <seamoon.dev@gmail.com>
2023-02-10 22:59:29 +0900

    init project

---
+++ src/main/java/com/seamoon/pjt/App.java
@@ -0,0 +1,13 @@
+package com.seamoon.pjt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
